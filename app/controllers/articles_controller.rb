96109b6e550c117b55247a619674506ac6273d3c
elliottjf <elliottj@pobox.com>
2017-05-13 00:47:29 -0700

    Tweak mainn search view.  Update CRUD to match correct fields

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -77,6 +77,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :published_on)
+      params.require(:article).permit(:title, :abstract, :url, :content, :published_on)
     end
 end
