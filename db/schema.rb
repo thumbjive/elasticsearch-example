6080c5a44e622e864569d0d8beecb5ba7cee604a
elliottjf <elliottj@pobox.com>
2017-05-12 17:45:12 -0700

    Added additional columns to Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170513004504) do
+ActiveRecord::Schema.define(version: 20170513004510) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20170513004504) do
     t.date "published_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "abstract"
+    t.string "url"
+    t.integer "shares"
   end
 
   create_table "articles_categories", force: :cascade do |t|
